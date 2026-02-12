3f77cdb26b5ea22c6a9f497999e16cd6ea2d660b
Pragati Jaiswal <pragatijuly7@gmail.com>
2026-02-13 01:33:48 +0530

    Initial DSA Upload

---
+++ JAVA/Practice/num.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class num
+{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the no.");
+        int n=sc.nextInt();
+        printnum(n);
+    }
+    static void printnum(int n)
+    {
+        if(n==0)
+            return;
+        else
+        {
+            printnum(n-1);
+            System.out.println(n);
+
+        }
+    }
+}
